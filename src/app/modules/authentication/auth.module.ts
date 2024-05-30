e3090c9e0f68337884d075ad53c8d3655726d440
RAMEES-KKVL <rameesp41750@gmail.com>
2024-05-30 13:29:38 +0530

    Implemented the full UI for the login page

--- src/app/modules/authentication/auth.module.ts
+++ src/app/modules/authentication/auth.module.ts
@@ -4,11 +4,16 @@ import { SignupBody } from "./pages/signup/signup.component";
 import { AuthHeaderComponent } from "./components/auth-header/auth-header.component";
 import { AuthSubmitButtonComponent } from "./components/auth-submit-button/auth-submitButton.component";
 import { MatInputModule } from "@angular/material/input";
+import { LoginBody } from "./pages/login/login.component";
 
 const routes: Routes = [
     {
         path: "signup",
         component : SignupBody
+    },
+    {
+        path : "login",
+        component : LoginBody
     }
 ]
 
@@ -16,7 +21,8 @@ const routes: Routes = [
     declarations: [
         SignupBody,
         AuthHeaderComponent,
-        AuthSubmitButtonComponent
+        AuthSubmitButtonComponent,
+        LoginBody
     ],
     imports: [
         RouterModule.forChild(routes),
---
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "auth-login-body",
+    templateUrl : "./login.component.html",
+    styleUrls : ["./login.component.css"]
+})
+export class LoginBody {
+    backgroundImage = 'assets/images/auth_bgimage_book_lap.jpg'
+}
