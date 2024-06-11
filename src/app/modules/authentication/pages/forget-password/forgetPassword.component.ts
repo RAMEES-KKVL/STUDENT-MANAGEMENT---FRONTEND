249a85baf9f44eaa92d1fe73a0e31547d3d8c2d7
RAMEES-KKVL <rameesp41750@gmail.com>
2024-06-11 20:55:08 +0530

    Added required field validation for reset password section

--- src/app/modules/authentication/pages/forget-password/forgetPassword.component.ts
+++ src/app/modules/authentication/pages/forget-password/forgetPassword.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector : "auth-forgetPassword-body",
@@ -7,4 +8,33 @@ import { Component } from "@angular/core";
 })
 export class ForgetPasswordBody {
     backgroundImage = 'assets/images/auth_bgimage_book_lap.jpg'
+
+    // Creating form and validating input fields
+    forgetPassForm: FormGroup
+    constructor(private fb: FormBuilder){
+        this.forgetPassForm = this.fb.group({
+            email: ["", [Validators.required, Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)]],
+        })
+    }
+
+    // Validating Email input 
+    emailError: boolean = false
+    emailTouched: boolean = false
+    emailErrorPattern: boolean | undefined = false
+
+    onEmailChange(event: Event){
+        const target = event.target as HTMLInputElement
+        this.emailTouched = true
+        this.emailError = target.value ? false : true
+        
+        if(target.value){            
+            this.emailErrorPattern = this.forgetPassForm.get('email')?.hasError('pattern')        
+        }
+        
+        setTimeout(() => {
+            this.emailTouched = false
+            this.emailError = false
+            this.emailErrorPattern = false
+        }, 3000);
+    }
 }
--- src/app/modules/authentication/pages/reset-password/resetPassword.component.ts
+++ src/app/modules/authentication/pages/reset-password/resetPassword.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from "@angular/forms";
 
 @Component({
     selector : "auth-resetPassword-body",
@@ -7,4 +8,67 @@ import { Component } from "@angular/core";
 })
 export class ResetPasswordBody {
     backgroundImage = 'assets/images/auth_bgimage_book_lap.jpg'
+
+    // Creating form and validating input fields
+    resetPassForm: FormGroup
+    constructor(private fb: FormBuilder){
+        this.resetPassForm = this.fb.group({
+            password: ["", Validators.compose([Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8}$/)])],
+            confirmPassword: ["", Validators.required, this.passwordMatchValidator()]
+        })
+    }
+
+    // Validating Password input 
+    passwordError: boolean = false
+    passwordTouched: boolean = false
+    passwordErrorPattern: boolean | undefined = false
+
+    onPasswordChange(event: Event){
+        const target = event.target as HTMLInputElement
+        this.passwordTouched = true
+        this.passwordError = target.value ? false : true
+
+        if(target.value){
+            this.passwordErrorPattern = this.resetPassForm.get('password')?.hasError('pattern')
+        }
+
+        setTimeout(()=>{
+            this.passwordError = false
+            this.passwordTouched = false
+            this.passwordErrorPattern = false
+        }, 3000)
+    }
+
+    // Validating Confirm password input
+    confirmPassError: boolean = false
+    confirmPassTouched: boolean = false
+    confirmPassPattern: boolean = false
+
+    onConfirmPasswordChange(event: Event){
+        const target = event.target as HTMLInputElement
+        this.confirmPassTouched = true
+        this.confirmPassError = target.value ? false : true
+
+        const password = this.resetPassForm?.get("password")?.value
+        const confirmPassword = this.resetPassForm?.get("confirmPassword")?.value
+        if(password !== confirmPassword){
+            this.confirmPassPattern = true
+        }
+
+        setTimeout(()=>{
+            this.confirmPassError = false
+            this.confirmPassTouched = false
+            this.confirmPassPattern = false
+        }, 3000)
+    }
+
+    // Function for checking is Confirm password is equal to Password
+    passwordMatchValidator(): ValidationErrors | null {
+        const password = this.resetPassForm?.get("password")?.value
+        const confirmPassword = this.resetPassForm?.get("confirmPassword")?.value
+        if(password !== confirmPassword){
+            return {"passwordMismatch" : true}
+        }
+        return null
+    }
 }
