7f6b7cef89519eb6718df7af171b69743e9ba3f7
RAMEES-KKVL <rameesp41750@gmail.com>
2024-06-26 17:22:35 +0530

    Completed homepage footer component UI design

---
+++ src/app/modules/sharedmodules/home-footer/homeFooter.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-home-footer",
+    templateUrl : "homeFooter.component.html"
+})
+export class HomeFooterComponent {}
--- src/app/modules/sharedmodules/shared.module.ts
+++ src/app/modules/sharedmodules/shared.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HomeHeaderComponent } from "./home-header/homeHeader.component";
+import { HomeFooterComponent } from "./home-footer/homeFooter.component";
 
 @NgModule({
     declarations: [
-        HomeHeaderComponent
+        HomeHeaderComponent,
+        HomeFooterComponent
     ],
     imports: [
         CommonModule,
     ],
     exports: [
-        HomeHeaderComponent
+        HomeHeaderComponent,
+        HomeFooterComponent
     ]
 })
 export class SharedModule {}
